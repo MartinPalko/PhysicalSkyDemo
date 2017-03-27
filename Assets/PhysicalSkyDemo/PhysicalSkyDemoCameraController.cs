7258dea47b22a55269695b75d689ddc8c2090a48
Martin Palko <jeeprubi@hotmail.com>
2017-03-26 23:11:31 -0400

    Added current demo files.

---
+++ Assets/PhysicalSkyDemo/PhysicalSkyDemoCameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalSkyDemoCameraController : MonoBehaviour
+{
+    public Vector2 mouseSensitivity = new Vector2(1.0f, 1.0f);
+
+    public float cameraHeight = 1.0f;
+    public float cameraDistance = 5.0f;
+
+    private Vector3 lastMousePosition;
+    private Vector2 cameraRoations;
+
+    private void Start()
+    {
+        lastMousePosition = Input.mousePosition;
+    }
+
+    void Update ()
+    {
+        // Mouse deltas
+        Vector3 mouseDelta = Input.mousePosition - lastMousePosition;
+        lastMousePosition = Input.mousePosition;
+
+        Vector2 moveDirection = mouseDelta;
+        moveDirection.Scale(mouseSensitivity);
+
+        if (Input.GetMouseButton(0))
+        {
+            // Camera Rotations
+            cameraRoations += moveDirection;
+            cameraRoations.y = Mathf.Clamp(cameraRoations.y, -30.0f, 80.0f);
+        }
+
+        // Position Camera
+        transform.position = new Vector3(Mathf.Sin(cameraRoations.x * Mathf.Deg2Rad) * cameraDistance, cameraHeight, Mathf.Cos(cameraRoations.x * Mathf.Deg2Rad) * cameraDistance);
+        transform.rotation = Quaternion.LookRotation(new Vector3(-transform.position.x, 0, -transform.position.z));
+        transform.Rotate(Vector3.left, cameraRoations.y, Space.Self);
+        
+
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+    }
+}
---
+++ Assets/PhysicalSkyDemo/PhysicalSkyDemoSceneController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+using PhysicalSky.Utilities;
+
+namespace PhysicalSky
+{
+    public class PhysicalSkyDemoSceneController : SunController
+    {
+        [SerializeField]
+        protected float latitude = 45.5017f;
+        [SerializeField]
+        protected float longitude = -73.5673f;
+        [SerializeField]
+        protected DateTime time = DateTime.Now;
+        [SerializeField]
+        protected TimeSpan timezone = TimeSpan.FromHours(5); // EST
+        [SerializeField]
+        protected float minutesPerHour = 60.0f;
+        [SerializeField]
+        protected float sunsetSpeedMultiplier = 0.2f;
+        [SerializeField]
+        protected float nightSpeedMultiplier = 10.0f;
+
+        float sunSpeedPct = 0.0f;
+
+        float minAltitude = 0.1f;
+        float maxAltitude = 100.0f;
+        float altitudeSpeed = 30.0f;
+        float altitudeSpeedPct = 0.0f;
+        float currentAltitude = 0.1f;
+
+        private void Update()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            float targetAltitudeSpeedPct = 0.0f;
+            if (Input.GetKey(KeyCode.UpArrow))
+                targetAltitudeSpeedPct += 1.0f;
+            if (Input.GetKey(KeyCode.DownArrow))
+                targetAltitudeSpeedPct -= 1.0f;
+
+            float altitudeChange = Mathf.Lerp(altitudeSpeedPct, targetAltitudeSpeedPct, Time.deltaTime * 5.0f) * currentAltitude * altitudeSpeed;
+            currentAltitude = Mathf.Clamp(currentAltitude + altitudeChange * Time.deltaTime, minAltitude, maxAltitude);
+            sky.Altitude = currentAltitude;
+
+
+            float targetSunSpeedPct = 0.0f;
+            if (Input.GetKey(KeyCode.RightArrow))
+                targetSunSpeedPct += 1.0f;
+            if (Input.GetKey(KeyCode.LeftArrow))
+                targetSunSpeedPct -= 1.0f;
+            
+            sunSpeedPct = Mathf.Lerp(sunSpeedPct, targetSunSpeedPct, Time.deltaTime * 5.0f);
+
+            float sunDeltaTime = Time.deltaTime * sunSpeedPct;
+
+            float sunsetY = -0.03f; // TODO: Find actual horizon angle based on altitude & planetary radius.
+            float sunSpeed = 1.0f;
+
+            sunSpeed *= sky.SunDirection.y < sunsetY - 0.1f ? nightSpeedMultiplier : 1.0f;
+            float distanceToSunset = Mathf.Clamp01(Mathf.Abs(sky.SunDirection.y - sunsetY));
+            sunSpeed *= Mathf.Lerp(sunsetSpeedMultiplier, 1.0f, distanceToSunset);
+
+            time = time.AddHours((sunDeltaTime) * sunSpeed * (60.0f / minutesPerHour));
+
+            double sunAltitude;
+            double sunAzimuth;
+            SunPosition.CalculateSunPosition(time, latitude, longitude, out sunAltitude, out sunAzimuth);
+
+            sky.SunDirection = CartesianCoords.SphericalToCartesian((float)sunAzimuth, (float)sunAltitude);
+        }
+
+    }
+}
